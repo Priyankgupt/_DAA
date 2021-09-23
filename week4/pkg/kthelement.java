aec54f928618e60e7b1c243164519de8b76249d8
Priyankgupt <67186956+Priyankgupt@users.noreply.github.com>
2021-09-23 18:50:52 +0530

    Update kthelement.java

--- week4/pkg/kthelement.java
+++ week4/pkg/kthelement.java
@@ -1,3 +1,27 @@
+/* Priyank Gupta B.tech CSE(SE) 2015092
+    Given an unsorted array of integers, design an algorithm and implement it using a program to find Kth smallest
+    or largest element in the array. (Worst case Time Complexity = O(n)).
+
+Input Format:
+
+The first line contains number of test cases, T
+For each test case, there will be three input lines.
+First line contains n (the size of array). Second line contains space-separated integers describing array. 
+Third line contains K.
+
+Output Format:
+The output will have T number of lines.
+For each test case, output will be the Kth smallest or largest array element. 
+If no Kih element is present, output should be "not present".
+
+Input:-
+1
+5 
+7 10 4  3 15
+
+Output :-
+7
+ */
 package pkg;
 import java.util.Arrays;
 import java.util.Collections;
